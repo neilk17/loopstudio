04a8639793f42787c1eb14557631660cf100f865
neilk17 <kanakianeil@gmail.com>
2022-11-25 21:39:49 -0500

    initial commit

---
+++ src/data-desktop.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+      id: 1,
+      image: 'desktop/image-deep-earth.jpg',
+      title: 'Deep Earth',
+    },
+    {
+      id: 2,
+      image: 'desktop/image-night-arcade.jpg',
+      title: 'Night arcade',
+    },
+    {
+      id: 3,
+      image: 'desktop/image-soccer-team.jpg',
+      title: 'Soccer team vr',
+    },
+    {
+      id: 4,
+      image: 'desktop/image-grid.jpg',
+      title: 'The Grid',
+    },
+    {
+      id: 5,
+      image: 'desktop/image-from-above.jpg',
+      title: 'from up above vr',
+    },
+    {
+      id: 6,
+      image: 'desktop/image-pocket-borealis.jpg',
+      title: 'pocket borealis',
+    },
+    {
+      id: 7,
+      image: 'desktop/image-curiosity.jpg',
+      title: 'the curiosity',
+    },
+    {
+      id: 8,
+      image: 'desktop/image-fisheye.jpg',
+      title: 'Make it fisheye',
+    },
+]
+
+export default data
---
+++ src/data-mobile.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+      id: 1,
+      image: 'mobile/image-deep-earth.jpg',
+      title: 'Deep Earth',
+    },
+    {
+      id: 2,
+      image: 'mobile/image-night-arcade.jpg',
+      title: 'Night arcade',
+    },
+    {
+      id: 3,
+      image: 'mobile/image-soccer-team.jpg',
+      title: 'Soccer team vr',
+    },
+    {
+      id: 4,
+      image: 'mobile/image-grid.jpg',
+      title: 'The Grid',
+    },
+    {
+      id: 5,
+      image: 'mobile/image-from-above.jpg',
+      title: 'from up above vr',
+    },
+    {
+      id: 6,
+      image: 'mobile/image-pocket-borealis.jpg',
+      title: 'pocket borealis',
+    },
+    {
+      id: 7,
+      image: 'mobile/image-curiosity.jpg',
+      title: 'the curiosity',
+    },
+    {
+      id: 8,
+      image: 'mobile/image-fisheye.jpg',
+      title: 'Make it fisheye',
+    },
+]
+
+export default data
